c4772e8b99f90c29c0742386560f7af42d6c58e5
Lawr <lawrsora@gmail.com>
2026-05-12 13:22:25 +0300

    12.05 end koworking

--- tracker-front/src/App.vue
+++ tracker-front/src/App.vue
@@ -1,5 +1,23 @@
 <template>
-  <div id="app">
-  <router-View />
-  </div>
+  <main class="main-light">
+      <router-View />
+      <div class="component">H</div>
+  </main>
 </template>
+
+<style scope>
+
+  main {
+    overflow: hidden;
+    width: 100%;
+    height: 100vh;
+  }
+
+  .main-light {
+    background-color: var(--background-light);
+  }
+
+  .component {
+    background-color: var(--component-background-light);
+  }
+</style>
